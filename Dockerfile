c2106620e5e617c9152df43cab65234e21518fc1
hochdyl <dylan.hochet50@gmail.com>
2023-01-06 14:59:46 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ WORKDIR /var/www
 RUN npm install -g supervisor
 # If you are building your code for production
 # RUN npm ci --only=production
-RUN npm i
+# RUN npm i
 
 EXPOSE 8000
 CMD [ "npm", "run", "watchserver" ]
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+const cors = require('cors')
 const server = express();
 const port = 8000;
 
@@ -13,6 +14,7 @@ server.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 server.use(bodyParser.json());
 
+server.use(cors);
 server.use(verifyToken);
 
 server.listen(port, () => {
